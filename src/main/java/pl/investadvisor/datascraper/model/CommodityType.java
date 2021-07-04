38c2c5601cf605278ef59d93fdb02398750ecd7f
RybickiMichal <rybickimichal2@gmail.com>
2021-07-04 15:11:12 +0200

    changes in commodity Types

--- src/main/java/pl/investadvisor/datascraper/model/CommodityType.java
+++ src/main/java/pl/investadvisor/datascraper/model/CommodityType.java
@@ -1,5 +1,5 @@
 package pl.investadvisor.datascraper.model;
 
 public enum CommodityType {
-    US_STOCK, PL_STOCK, ETF, CRYPTO, METAL, INDUSTRIAL_RESOURCE, RARE_EARTH, AGRO, REIT
+    US_STOCK, PL_STOCK, ETF, CRYPTO, PRECIOUS_METAL, INDUSTRIAL_RESOURCE, AGRO, REIT
 }
