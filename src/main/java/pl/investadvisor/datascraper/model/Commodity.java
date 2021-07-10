fa59abf739fbbc70db761e562ecb850d21571eb0
RybickiMichal <rybickimichal2@gmail.com>
2021-07-10 15:17:18 +0200

    add IndustrialRawMaterial enum and comment field

--- src/main/java/pl/investadvisor/datascraper/model/Commodity.java
+++ src/main/java/pl/investadvisor/datascraper/model/Commodity.java
@@ -42,11 +42,17 @@ public class Commodity {
     @NonNull
     private CommoditySector commoditySector;
 
+    @DynamoDBTypeConvertedEnum
+    private IndustrialRawMaterial industrialRawMaterial;
+
     @NonNull
     private String currency;
 
     @NonNull
     private BigDecimal price;
+
+    @NonNull
+    private String comment;
 
     @DynamoDBTypeConvertedTimestamp(pattern = "yyyyMMddHHmmssSSS")
     @NonNull
